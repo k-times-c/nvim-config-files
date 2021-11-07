c1a3367701a4309cf5631ae29f14ad3dbf64dfd7
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-11-07 00:52:31 -0700

    add nowait key mappings for 'dd', 'cc', 'yy' so vim-surround can be loaded lazily without any issues

--- lua/my_plugins/for/editing/normal/surround.lua
+++ lua/my_plugins/for/editing/normal/surround.lua
@@ -1,1 +1,8 @@
-use { 'tpope/vim-surround', opt = true, keys =  {'ys', 'ds', 'cs', 'S' }}
+
+if vim.fn.exists("loaded_surround") == 0 or vim.g.loaded_surround ~= 1 then
+	local nowait_noremap = function(keymap) vim.api.nvim_set_keymap("n", keymap, "<cmd>normal! " .. keymap .. "<cr>", { noremap = true, nowait = true } ) end
+	for _, key in ipairs({"yy", "dd", "cc"}) do
+		nowait_noremap(key)
+	end
+end
+use { 'tpope/vim-surround', opt = true, keys =  {'ys', 'ds', 'cs', { 'v', 'S' }}}
--- lua/my_plugins/for/uxui/which-key.lua
+++ lua/my_plugins/for/uxui/which-key.lua
@@ -1,17 +1,3 @@
 -- Lua
-use {
-"folke/which-key.nvim",
-config = function()
-require("which-key").setup {
-	triggers_blacklist = {
-		-- list of mode / prefixes that should never be hooked by WhichKey
-		-- this is mostly relevant for key maps that start with a native binding
-		-- most people should not need to change this
-		n = { "d" },
-		i = { "j", "k" },
-		v = { "j", "k" },
-	},
-}
-end
-}
+use { "folke/which-key.nvim", config = function() require("which-key").setup {} end }
 
