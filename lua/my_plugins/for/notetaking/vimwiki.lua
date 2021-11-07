f901de2a3bc6d66a9114c1fa5a10f8f010e52712
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-11-07 01:16:10 -0700

    use vimwiki's markdown features versus vimwiki flavor of plugin, and adding another keymap for lazy loading in packer

--- lua/my_plugins/for/notetaking/vimwiki.lua
+++ lua/my_plugins/for/notetaking/vimwiki.lua
@@ -1,10 +1,11 @@
-require('packer').use { 'vimwiki/vimwiki', opt = true, keys = "<leader>ww", "<Leader>wt", ft = "vimwiki" }
+require('packer').use { 'vimwiki/vimwiki', opt = true, keys = {"<leader>ww", "<Leader>wt"}, ft = "vimwiki" }
 -- vim.g.wiki_list = [[[{'path': '~/my_wiki/'}]]]
 -- vim.api.nvim_set_var('wiki_list', [[ [{'path': '~/my_wiki/'}] ]])
-vim.g.vimwiki_path = '~/wiki'
+-- vim.g.vimwiki_path = '~/wiki'
 --[[ vim.api.set_keymap('n', '<leader>wp', 'Denite file -path=' .. vim.g.vimwiki_path .. '/projects', nnoremap = true)
 vim.api.set_keymap('n', '<leader>wm', 'Denite file -path=' .. vim.g.vimwiki_path .. '/meetings', nnoremap = true) ]]
 
+vim.cmd([[ let g:vimwiki_list = [{'path': '~/wiki/', 'syntax': 'markdown', 'ext': '.md'}] ]])
 --[[ " p - projects  {{{
 nnoremap ,wp :call execute('Denite file -path=' . g:vimwiki_path . '/projects')<CR>
 " }}}
