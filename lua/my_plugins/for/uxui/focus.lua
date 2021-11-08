260386499c440297be3d9dad08d01c78de3cc8d5
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-11-07 17:53:37 -0800

    remove rougue line from focus config function

--- lua/my_plugins/for/uxui/focus.lua
+++ lua/my_plugins/for/uxui/focus.lua
@@ -2,7 +2,5 @@ use { "beauwilliams/focus.nvim", opt = true,
 	cmd = { "FocusNicely", "FocusToggle", "FocusEnable" },
 	config = function() 
 	      require("focus").setup() 
-		local focusmap = function(direction)
-	end
 end
 }
