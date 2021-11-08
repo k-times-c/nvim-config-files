33996ff9ec860ca8f2b9a5adf659b8215d5e19b4
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-11-07 21:34:17 -0800

    map `Y` to `yg_`

--- lua/mappings/normal.lua
+++ lua/mappings/normal.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_keymap('n', 'gV', '`[v`]', { noremap = true } )
 vim.api.nvim_set_keymap("n", "gf", "<cmd>normal! vi'gf<cr>", { noremap = true } )
+vim.api.nvim_set_keymap("n", "Y", "yg_", { noremap = true } )
