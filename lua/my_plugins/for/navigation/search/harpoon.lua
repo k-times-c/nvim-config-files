f5b469f1c10e7be37d8bff42771ac2d1c807e177
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-11-07 21:34:56 -0800

    move harpoon mappings into setup function

--- lua/my_plugins/for/navigation/search/harpoon.lua
+++ lua/my_plugins/for/navigation/search/harpoon.lua
@@ -1,9 +1,10 @@
 use { "ThePrimeagen/harpoon", opt = true, keys = {"<leader>ho", "<leader>ha",  "<leader>ht", "<leader>hsc"},
 	requires = { 'nvim-lua/plenary.nvim'},
-	config = function() require("harpoon").setup() end
+	config = function() require("harpoon").setup() 
+	--- personal mappings
+	vim.api.nvim_set_keymap('n', "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', "<leader>ho", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', "<leader>ht", "<cmd>lua require('harpoon.term').gotoTerminal(1)<CR>", { noremap = true, silent = true })
+	end
 }
 
---- personal mappings
-vim.api.nvim_set_keymap('n', "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', "<leader>ho", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', "<leader>ht", "<cmd>lua require('harpoon.term').gotoTerminal(1)<CR>", { noremap = true, silent = true })
