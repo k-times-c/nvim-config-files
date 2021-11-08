c67d9da78cb1cce8bd4a1988f8aadd6c7789c407
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-11-07 21:36:23 -0800

    add and lazyload vim-dirvish-git, and vim-dirvinist plugins

--- lua/my_plugins/for/navigation/file_exploring/vim-dirvish.lua
+++ lua/my_plugins/for/navigation/file_exploring/vim-dirvish.lua
@@ -1,2 +1,6 @@
-require('packer').use {"justinmk/vim-dirvish"}
-require('packer').use { "fsharpasharp/vim-dirvinist" }
+use {"justinmk/vim-dirvish"}
+use { "fsharpasharp/vim-dirvinist", opt = true, fn = "ProjectionistDetect" }
+use { "kristijanhusak/vim-dirvish-git", opt = true, ft = 'dirvish', config = function()
+	vim.g.dirvish_git_show_ignored = 1
+end
+}
