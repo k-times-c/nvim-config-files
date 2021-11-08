a125655220397b7aeaf32360fb665f266a85075f
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-11-07 21:28:53 -0800

    add `Formatter.nvim` plugin w/ python, lua, and hcl conf

---
+++ lua/my_plugins/for/editing/formatter.lua
@@ -0,0 +1,41 @@
+use {'mhartington/formatter.nvim', config = function() 
+	 require('formatter').setup {
+		 -- TODO: somehow get this to play nice with the internal equalprg and formatprg programs
+		 -- vim.g.formatexpr =Format
+		 filetype = {
+			 lua = {
+				 -- luafmt
+				 function()
+					 return {
+						 exe = "luafmt",
+						 args = {"--indent-count", 2, "--stdin"},
+						 stdin = true
+					 }
+				 end
+			 },
+			 terraform = {
+				 function()
+					 return {
+						 exe = "terraform",
+						 args = { "fmt", "-" },
+						 stdin = true
+					 }
+				 end
+			 },
+			 python = {
+				 function()
+					 return {
+						 exe = "python3 -m autopep8",
+						 args = {
+							 "--in-place --aggressive --aggressive",
+							 vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
+						 },
+						 stdin = false
+					 }
+				 end
+			 }
+		 }
+	 }
+	 vim.api.nvim_set_keymap('n', 'gq', ':Format<CR>', { noremap = true } )
+ end
+ }
--- lua/my_plugins/for/editing/init.lua
+++ lua/my_plugins/for/editing/init.lua
@@ -1,3 +1,4 @@
 require('my_plugins/for/editing/cmd-line')
 require('my_plugins/for/editing/normal')
 require('my_plugins/for/editing/windows-management')
+require('my_plugins/for/editing/formatter')
