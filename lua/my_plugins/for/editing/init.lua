14bc3d700f205190ab69e1a07e5d6a1ed9d75ee6
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-11-07 19:36:58 -0800

    add TrueZen plugin for more focused editing

--- lua/my_plugins/for/editing/init.lua
+++ lua/my_plugins/for/editing/init.lua
@@ -1,2 +1,3 @@
 require('my_plugins/for/editing/cmd-line')
 require('my_plugins/for/editing/normal')
+require('my_plugins/for/editing/windows-management')
---
+++ lua/my_plugins/for/editing/windows-management.lua
@@ -0,0 +1,13 @@
+use { "Pocco81/TrueZen.nvim", config = function()
+	require('true-zen').setup {
+		integrations = {
+			gitsigns = true,
+			nvim_bufferline = true,
+			feline = true
+		}
+	}
+	vim.api.nvim_set_keymap('n', 'ZM', '<Cmd>TZMinimalist<CR>', { noremap = true })
+	vim.api.nvim_set_keymap('n', 'ZF', '<Cmd>TZFocus<CR>', { noremap = true })
+	vim.api.nvim_set_keymap('n', 'ZA', '<Cmd>TZAtaraxis<CR>', { noremap = true })
+	end
+}
