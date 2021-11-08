b89c5be8e13cbdb91725450c5ef95a71df98ca69
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-11-07 21:42:03 -0800

    add TrueZen plugin for more focused editing
    add and lazy load TrueZen

--- lua/my_plugins/for/editing/windows-management.lua
+++ lua/my_plugins/for/editing/windows-management.lua
@@ -1,4 +1,4 @@
-use { "Pocco81/TrueZen.nvim", config = function()
+use { "Pocco81/TrueZen.nvim", opt = true, keys = { 'ZM', 'ZF', 'ZA' }, config = function()
 	require('true-zen').setup {
 		integrations = {
 			gitsigns = true,
