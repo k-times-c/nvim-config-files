9f9863257193d37ae2d66a3907db477ac546bbfb
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-11-07 17:26:03 -0800

    fixup! add vim-dirvish for file exploring

--- lua/my_plugins/for/navigation/file_exploring/vim-dirvish.lua
+++ lua/my_plugins/for/navigation/file_exploring/vim-dirvish.lua
@@ -1,3 +1,1 @@
-require('packer').use{"justinmk/vim-dirvish", config = function ()
-	vim.cmd([[call dirvish#add_icon_fn({p -> p[-1:]=='/'?'📂':'📄'}]])
-end}
+require('packer').use{"justinmk/vim-dirvish"}
