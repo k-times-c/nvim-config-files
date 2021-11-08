e003e39c6990063375e8505dd0ab4a907f679e1f
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-11-07 17:55:14 -0800

    add exchange keys (i.e. `[e` and `]e`) to packer lazy load for unimpaired

--- lua/my_plugins/for/navigation/unimpaired.lua
+++ lua/my_plugins/for/navigation/unimpaired.lua
@@ -1,1 +1,1 @@
-use { "tpope/vim-unimpaired", opt = true, keys = { "[A", "]A", "[a", "]a", "[b", "]b", "[ ", "] " } }
+use { "tpope/vim-unimpaired", opt = true, keys = { "[A", "]A", "[a", "]a", "[b", "]b", "[ ", "] ", "[e", "]e" } }
