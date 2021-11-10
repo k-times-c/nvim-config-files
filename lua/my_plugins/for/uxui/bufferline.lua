7d6536183e5738ea253d4ab8750db1d054deecf9
Kyle X. Clark <kyle.clark@nordstrom.com>
2021-11-09 17:48:07 -0800

    add logic to filter out wiki lines from bufferline

--- lua/my_plugins/for/uxui/bufferline.lua
+++ lua/my_plugins/for/uxui/bufferline.lua
@@ -2,7 +2,14 @@ use { 'akinsho/bufferline.nvim',
 	requires = 'kyazdani42/nvim-web-devicons',
 	config = function() 
 		require('bufferline').setup {
-			diagnostics = "nvim_lsp"
+			options = {
+				diagnostics = "nvim_lsp",
+				custom_filter = function(buf_number) 
+					if vim.bo[buf_number].filetype ~= "vimwiki" then
+						return true
+					end
+				end
+			}
 		}
 		end
 }
