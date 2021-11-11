5134d79092e7d841ec27ffb7b581a5bd5cc18e2d
Kyle X. Clark <kyle.clark@nordstrom.com>
2021-11-11 10:52:49 -0800

    change vimwiki home dir depending on my computer

--- lua/my_plugins/for/notetaking/vimwiki.lua
+++ lua/my_plugins/for/notetaking/vimwiki.lua
@@ -4,8 +4,14 @@ require('packer').use { 'vimwiki/vimwiki', opt = true, keys = {"<leader>w<leader
 -- vim.g.vimwiki_path = '~/wiki'
 --[[ vim.api.set_keymap('n', '<leader>wp', 'Denite file -path=' .. vim.g.vimwiki_path .. '/projects', nnoremap = true)
 vim.api.set_keymap('n', '<leader>wm', 'Denite file -path=' .. vim.g.vimwiki_path .. '/meetings', nnoremap = true) ]]
-
-vim.cmd([[ let g:vimwiki_list = [{'path': '~/wiki/', 'syntax': 'markdown', 'ext': '.md'}] ]])
+local wiki_parent_dir = ""
+if vim.fn.has("Mac") == 1 then
+	vim.cmd([[ let g:vimwiki_list = [{'path': '~/wiki/', 'syntax': 'markdown', 'ext': '.md'}] ]])
+else
+	wiki_parent_dir =  'C:/Users/gruq/OneDrive - Nordstrom'
+	local wiki_settings = [[ [{'path': ']] .. wiki_parent_dir .. [[/wiki/', 'syntax': 'markdown', 'ext': '.md'}] ]]
+	vim.cmd("let g:vimwiki_list = " .. wiki_settings)
+end
 --[[ " p - projects  {{{
 nnoremap ,wp :call execute('Denite file -path=' . g:vimwiki_path . '/projects')<CR>
 " }}}
