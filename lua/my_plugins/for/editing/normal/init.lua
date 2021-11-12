32d6a14e8af07ea73b36e3e9eb9aee42b707d381
Kyle X. Clark <kyle.clark@nordstrom.com>
2021-11-11 17:01:46 -0800

    install vim-repeat

--- lua/my_plugins/for/editing/normal/init.lua
+++ lua/my_plugins/for/editing/normal/init.lua
@@ -1,4 +1,5 @@
 require('my_plugins/for/editing/normal/commenting')
 require('my_plugins/for/editing/normal/surround')
+require('my_plugins/for/editing/normal/vim-repeat')
 require('my_plugins/for/editing/normal/neoclip')
 require('my_plugins/for/editing/normal/surround')
