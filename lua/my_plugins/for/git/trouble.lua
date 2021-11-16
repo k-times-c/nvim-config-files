ca74e300709fd3a8b7573b068ecfcc2a58b6ba73
Kyle X. Clark <kyle.clark@nordstrom.com>
2021-11-16 00:36:50 -0500

    refactor: change double quotes to single

--- lua/my_plugins/for/git/trouble.lua
+++ lua/my_plugins/for/git/trouble.lua
@@ -1,7 +1,6 @@
-
 use {
-  "folke/trouble.nvim",
-  requires = "kyazdani42/nvim-web-devicons",
+  'folke/trouble.nvim',
+  requires = 'kyazdani42/nvim-web-devicons',
   config = function() 
 	 require("trouble").setup{} 
   end
