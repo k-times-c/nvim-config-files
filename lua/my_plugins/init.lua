151a39b14f11d742815432e1a2db74c03bc7a7cf
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-12-05 19:22:06 -0800

    install impatient.nvim

--- lua/my_plugins/init.lua
+++ lua/my_plugins/init.lua
@@ -5,33 +5,39 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 use = require('packer').use
-return require('packer').startup(function(use)
+return require('packer').startup({function(use)
 
--- TODO: pull out to its own file
-  use {'wbthomason/packer.nvim'}
--- TODO: pull out to its own file
--- TODO: implement for json files
-  -- use { 'gennaro-tedesco/nvim-jqx', filetype = 'json' }
-  use { 'andymass/vim-matchup', opt = true, keys = '%' }
-  use { 'tommcdo/vim-exchange' }
-  use { 'wellle/targets.vim' }
-  require('my_plugins/for/uxui')
-  require('my_plugins/for/lsp')
-  if vim.fn.has('Mac') == 1 then
-	require('my_plugins/for/treesitter')
-  end
-  require('my_plugins/for/git')
-  require('my_plugins/for/navigation')
-  require('my_plugins/for/notetaking')
-  require('my_plugins/for/snippet-support')
-  require('my_plugins/for/testing')
-  require('my_plugins/for/code-running')
-  require('my_plugins/for/editing')
-  require('my_plugins/for/debugging')
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+  -- TODO: pull into to its own file
+   use 'lewis6991/impatient.nvim'
+   require('packer_compiled')
+   use {'wbthomason/packer.nvim'}
+  -- TODO: pull into to its own file
+  -- TODO: implement for json files
+    -- use { 'gennaro-tedesco/nvim-jqx', filetype = 'json' }
+    use { 'andymass/vim-matchup', opt = true, keys = '%' }
+    use { 'tommcdo/vim-exchange' }
+    use { 'wellle/targets.vim' }
+    require('my_plugins/for/uxui')
+    require('my_plugins/for/lsp')
+    if vim.fn.has('Mac') == 1 then
+  	require('my_plugins/for/treesitter')
+    end
+    require('my_plugins/for/git')
+    require('my_plugins/for/navigation')
+    require('my_plugins/for/notetaking')
+    require('my_plugins/for/snippet-support')
+    require('my_plugins/for/testing')
+    require('my_plugins/for/code-running')
+    require('my_plugins/for/editing')
+    require('my_plugins/for/debugging')
+  
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+      require('packer').sync()
+    end
+  end,
+  config = {
+  	compile_path = vim.fn.stdpath('config')..'/lua/packer_compiled.lua'
+  }
+})
