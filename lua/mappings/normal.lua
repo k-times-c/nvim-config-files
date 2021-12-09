483529d966c534c4d1848d00856dcf331c0ea3a2
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-12-08 22:49:53 -0800

    add new custom mapping executing files

--- lua/mappings/normal.lua
+++ lua/mappings/normal.lua
@@ -6,10 +6,10 @@ vim.cmd([[
 
 vim.api.nvim_set_keymap('n', 'gV', '`[v`]', { noremap = true } )
 vim.api.nvim_set_keymap('n', 'gf', "<cmd>normal! vi'gf<cr>", { noremap = true } )
-vim.api.nvim_set_keymap('n', 'Y', "yg_", { noremap = true } )
 vim.api.nvim_set_keymap('n', 'y<C-g>', '<cmd>let @"=@%<CR>', { noremap = true } )
 vim.api.nvim_set_keymap('n', '<C-s>', '<cmd>update<CR>', { noremap = true } )
-vim.api.nvim_set_keymap('n', ',x', ':normal lua ', { noremap = true } )
+vim.api.nvim_set_keymap('n', ',x', ':normal lua <CR>', { noremap = true } )
+vim.api.nvim_set_keymap('n', ',,x', ':normal lua <CR>', { noremap = true } )
 vim.api.nvim_set_keymap('v', '<BS>', '"_d', { noremap = true } )
 --[[ vim.cmd( [[
 	function! kxclark#StripTrailingWhitespaces()
@@ -27,9 +27,3 @@ vim.api.nvim_set_keymap('v', '<BS>', '"_d', { noremap = true } )
 	nnoremap ,sws <cmd>call kxclark#StripTrailingWhitespaces()<cr>
 )
 ]]
-
-vim.api.nvim_set_keymap('n', 't<C-n>', '<cmd>TestNearest<CR>', { noremap = true } )
-vim.api.nvim_set_keymap('n', 't<C-f>', '<cmd>TestFile<CR>', { noremap = true } )
-vim.api.nvim_set_keymap('n', 't<C-s>', '<cmd>TestSuite<CR>', { noremap = true } )
-vim.api.nvim_set_keymap('n', 't<C-l>', '<cmd>TestLast<CR>', { noremap = true } )
-vim.api.nvim_set_keymap('n', 't<C-g>', '<cmd>TestVisit<CR>', { noremap = true } )
--- lua/my_plugins/for/uxui/todo-comments.lua
+++ lua/my_plugins/for/uxui/todo-comments.lua
@@ -2,7 +2,6 @@ use { 'folke/todo-comments.nvim',
 requires = "nvim-lua/plenary.nvim",
 config = function()
 	-- TODO: Configure with windows setup
-	require("todo-comments").setup {
-	}
+	require("todo-comments").setup { }
 end
 }
