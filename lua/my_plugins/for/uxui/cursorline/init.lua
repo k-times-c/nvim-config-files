6c4763f03319db0b5ae144153071d37887984c8e
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-12-26 17:23:27 -0500

    lazy load Pomodoro

--- lua/my_plugins/for/uxui/cursorline/init.lua
+++ lua/my_plugins/for/uxui/cursorline/init.lua
@@ -1,4 +1,5 @@
 -- require('my_plugins/for/uxui/cursorline/feline')
+-- TODO: reload and add pomodoro within the status line when loaded
 use {'nvim-lualine/lualine.nvim', config = function()
     -- require('lualine').setup { sections = { lualine_c = { 'filename', require('pomodoro').statusline() } } }
 end
