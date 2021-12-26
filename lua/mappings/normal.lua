890a5c60371c28f9b7d8ccbcb18d78aa224d523e
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-12-26 17:47:53 -0500

    add more normal mappings

--- lua/mappings/normal.lua
+++ lua/mappings/normal.lua
@@ -9,10 +9,11 @@ vim.api.nvim_set_keymap('n', 'gV', '`[v`]', { noremap = true } )
 -- vim.api.nvim_set_keymap('n', 'gf', "<cmd>normal! vi'gf<cr>", { noremap = true } )
 vim.api.nvim_set_keymap('n', 'y<C-g>', [[<cmd>let @" = expand('%:p')<cr>]], { noremap = true } )
 vim.api.nvim_set_keymap('n', '<C-s>', '<cmd>update<CR>', { noremap = true } )
-vim.api.nvim_set_keymap('n', ',x', ':normal lua vim.cmd(vim.api.nvim_get_current_line())<CR>', { noremap = true } )
+vim.api.nvim_set_keymap('n', ',x', ':lua vim.cmd(vim.inspect(vim.api.nvim_get_current_line()))<CR>', { noremap = true } )
 vim.api.nvim_set_keymap('n', ',,x', ':luafile %<CR>', { noremap = true } )
 vim.api.nvim_set_keymap('v', '<BS>', '"_d', { noremap = true } )
 vim.api.nvim_set_keymap('n', 'Z<BS>', '<cmd>Bdelete!<CR>', { noremap = true } )
+vim.api.nvim_set_keymap('n', '<bslash>q', '<cmd>cwindow<CR>', { noremap = true } )
 
 
 -- move to other buffers
@@ -20,21 +21,3 @@ vim.api.nvim_set_keymap('n', '<C-h>', '<C-w><C-h>', { noremap = true } )
 vim.api.nvim_set_keymap('n', '<C-j>', '<C-w><C-j>', { noremap = true } )
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w><C-k>', { noremap = true } )
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w><C-l>', { noremap = true } )
-
-
---[[ vim.cmd( [[
-	function! kxclark#StripTrailingWhitespaces()
-		"taken from vimcasts
-		" Preparation: save last search, and cursor position.
-		let _s=@/
-		let l = line(".")
-		let c = col(".")
-		" Do the business:
-		%s/\s\+$//e
-		" Clean up: restore previous search history, and cursor position
-		let @/=_s
-		call cursor(l, c)
-	endfunction
-	nnoremap ,sws <cmd>call kxclark#StripTrailingWhitespaces()<cr>
-)
-]]
