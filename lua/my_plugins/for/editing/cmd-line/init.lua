d3001f07a1f38fdd13b2201b5042d87c747d70b1
Kyle X. Clark <kyle.x.clark@gmail.com>
2021-12-26 16:45:34 -0500

    remove range-highlight plugin

--- lua/my_plugins/for/editing/cmd-line/init.lua
+++ lua/my_plugins/for/editing/cmd-line/init.lua
@@ -1,3 +1,1 @@
 require('my_plugins/for/editing/cmd-line/numb')
-require('my_plugins/for/editing/cmd-line/range-highlight')
--- require('my_plugins/for/editing/cmd-line/wilder')
--- lua/my_plugins/for/editing/cmd-line/range-highlight.lua
+++
@@ -1,9 +0,0 @@
-use 'winston0410/cmd-parser.nvim'
-use {'winston0410/range-highlight.nvim',
-	requires = {'winston0410/cmd-parser.nvim'},
-	opt = true,
-	event = 'CmdlineEnter',
-	config = function() 
-		require("range-highlight").setup {}
-	end,
-}
