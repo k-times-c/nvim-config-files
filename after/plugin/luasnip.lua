00816e2f9204c5b9ad89510c8c480b5fac6fb0df
Kyle X. Clark <kyle.x.clark@gmail.com>
2022-07-18 09:52:28 +0100

    adding more plugins for "PDE" (TM)

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,26 +1,56 @@
-
--- local ls = require('luasnip')
--- ls.snippets = {
--- 	all = {
---  		ls.parser.parse_snippet("expand", "here your first snippet K!")
--- 	},
--- 	lua = {}
--- }
-
-vim.keymap.set('n', "<space><space>s", function()
-  local plenary_reload = require('plenary.reload')
-  plenary_reload.reload_module("luasnip")
-  -- packer_plugins["LuaSnip"].loaded = false
-  require("packer").loader("LuaSnip")
-  vim.cmd("source ~/.config/nvim/after/plugin/luasnip.lua")
-end)
+vim.keymap.set('n', "<space><space>s",
+  "<cmd>update ~/.config/nvim/after/plugin/luasnip.lua | source   ~/.config/nvim/after/plugin/luasnip.lua | LuaSnipListAvailable<cr>")
 
 local ls = require('luasnip')
 
-ls.add_snippets("all", {
-	ls.parser.parse_snippet("expand", "here your first snippet K!"),
-	ls.parser.parse_snippet("n", "somethin thing"),
-	ls.parser.parse_snippet("j", "somethin thing"),
-	ls.parser.parse_snippet("k", "somethin thing"),
-})
+local function simple_restore(args, _)
+  return ls.sn(nil, { ls.i(1, args[1]), ls.r(2, "dyn", ls.i(nil, "user_text")) })
+end
 
+-- local function simple_restore(args, _)
+--   return ls.sn(nil, { ls.i(1, args[1]), ls.i(2, "user_text") })
+-- end
+--
+local snippet_collection = require('luasnip.session.snippet_collection')
+snippet_collection.clear_snippets("lua")
+ls.add_snippets("lua", {
+  ls.parser.parse_snippet("j", "here $1 we are my ${2:friends}"),
+  -- ls.parser.parse_snippet("j", "in west philadephia born and raise"),
+  ls.s("t", {
+    ls.i(1),
+    ls.f(function(args)
+      print("evalu")
+      vim.pretty_print(args)
+      -- vim.pretty_print(snip)
+      -- vim.pretty_print(user_arg_1)
+      return "[[[[[" .. args[1][1] .. "]]]]]"
+    end,
+      { 1 },
+      { user_args = { "Will be appended to text from i(0)" } }),
+    -- ls.i(0)
+  }),
+  ls.s("k", {
+    ls.t "text: ", ls.i(1), ls.t { "", "copy: " },
+    ls.d(2, function(args)
+      -- the returned snippetNode doesn't need a position; it's inserted
+      -- "inside" the dynamicNode.
+      return ls.sn(2, { ls.i(1, args[1]), ls.t { "", "something else", "" }, ls.i(2), ls.t { "", "hello" } })
+    end,
+      { 1 })
+  }),
+  ls.s("p", {
+    ls.c(1, {
+      ls.sn(nil, { ls.t("("), ls.r(1, "it_diffrent"), ls.t(")") }),
+      ls.sn(nil, { ls.t("["), ls.r(1, "it_diffrent"), ls.t("]") }),
+      ls.sn(nil, { ls.t("{"), ls.r(1, "it_diffrent"), ls.t("}") }),
+    }),
+  }, {
+    stored = {
+      it_diffrent = ls.i(1, "default set")
+    }
+  }),
+  ls.s("rest", {
+    ls.i(1, "preset"), ls.t { "", "" },
+    ls.d(2, simple_restore, 1)
+  }),
+})
--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 -- vim.g.loaded_netrwPlugin = true
+vim.g.do_filetype_lua          = 1 -- will be default in nvim 0.8
+vim.g.did_load_filetypes       = 0
 vim.g.loaded_tarPlugin         = true
 vim.g.loaded_zipPlugin         = true
 vim.g.loaded_tutor_mode_plugin = true
@@ -12,32 +14,27 @@ vim.o.swapfile                 = false
 vim.o.hidden                   = true
 vim.o.wrap                     = false
 vim.o.ruler                    = true
-vim.go.path                    = ".,,**3"
+-- vim.go.path                    = ".,,**3"
 vim.o.encoding                 = "utf8"
+vim.keymap.set('n', '<Space>', '<nop>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-w><C-w>', '<nop>', { noremap = true, silent = true }) -- windows meta key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.keymap.set('n', 'gV', '`[v`]')
 
--- Set highlight on search
 vim.o.hlsearch = false
-
--- Make line numbers default
 vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
---Enable relative number
+vim.o.mouse = 'a' -- Enable mouse mode
 vim.o.relativenumber = false
-
---Enable break indent
-vim.o.breakindent = true
+vim.o.breakindent = true --Enable break indent
 
 --Save undo history
-vim.o.history = 700
 vim.opt.undofile = true
+vim.o.history = 700
 
 --Case insensitive searching UNLESS /C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
 --Decrease update time
 vim.o.updatetime = 250
 vim.wo.signcolumn = 'yes'
@@ -48,11 +45,18 @@ vim.o.termguicolors = true
 
 -- add list symbols
 vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
+-- vim.opt.listchars:append("space:⋅")
+-- vim.opt.listchars:append("eol:↴")
 -- vim.opt.listchars:remove("tab:>")
--- vim.opt.listchars:append("tab:»")
--- vim.opt.listchars.set('eol:↲,tab:»,trail:·,extends:<,precedes:>,conceal:┊,nbsp:␣')
+-- vim.opt.listchars:append("tab:»"
+-- vim.opt.listchars = { 'eol:↲', 'tab:»', 'trail:-', 'extends:<', 'precedes:>', 'conceal:┊', 'nbsp:␣' }
+
+-- local lua_formatting = vim.api.nvim_create_augroup("Lua formatting", { clear = true })
+-- vim.api.nvim_create_autocmd("VimBufEnter",
+--     { pattern = "*.lua",
+--         callback = function() vim.cmd( [[ set softtab ]]) end,
+--     group = lua_formatting
+--     })
 
 local group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost",
@@ -60,12 +64,13 @@ vim.api.nvim_create_autocmd("TextYankPost",
     callback = function() vim.highlight.on_yank() end,
     group = group
   })
-vim.o.wildignore = "**/node_modules/**,**/dist/**,*.pyc,*__pycache__/**,**/venv/**,.DS_Store*"
+vim.opt.wildignore = { "**/node_modules/**", "**/dist/**", "*.pyc", "*__pycache__/**", "**/venv/**", ".DS_Store*" }
 
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+    install_path })
 end
 
 return require('packer').startup(function(use)
@@ -80,12 +85,26 @@ return require('packer').startup(function(use)
     vim.keymap.set('n', '<leader>pp', ':PackerProfile<cr>')
   end
   }
-
+  -- Lua
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
   use { 'bronson/vim-visual-star-search', opt = true, keys = { '#', ' #', '*', '<leader>*' } }
   use { 'andymass/vim-matchup', keys = "%" }
   use { 'gennaro-tedesco/nvim-jqx', opt = true, filetype = 'json' }
-  use { 'tpope/vim-surround' }
-  use { 'tpope/vim-abolish', cmd = { "Subvert", "Abolish", "S" } }
+  use({ "kylechui/nvim-surround",
+    config = function() require("nvim-surround").setup({}) end,
+    -- keys = { 'o', 'ys', 'cs', 'ds' } TODO: implement this with whichkey enabled
+  }
+  )
+  use { 'tpope/vim-abolish', cmd = { "Subvert", "Abolish", "S" }, keys = { { 'o', 'cr' } } }
   use { "tpope/vim-repeat", keys = '.' }
   use { 'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate',
@@ -94,18 +113,19 @@ return require('packer').startup(function(use)
       { "nvim-treesitter/nvim-treesitter-textobjects" },
       { 'nvim-treesitter/nvim-treesitter-context', after = "nvim-treesitter", disable = true },
       { 'nvim-treesitter/nvim-treesitter-refactor', keys = "grr" },
-      { 'RRethy/nvim-treesitter-endwise', ft = 'lua' },
+      -- { 'RRethy/nvim-treesitter-endwise', ft = 'lua' },
       -- TODO: wrap config call to be ran at the correct time?
-      { 'RRethy/nvim-treesitter-textsubjects', keys = { 'v', "." }, config = function() vim.cmd(":e | normal gv.") end }
+      { 'RRethy/nvim-treesitter-textsubjects', keys = { 'v', "<CR>" }, config = "<cmd>e | normal gv<cr>" }
     },
     config = function()
       require 'nvim-treesitter.configs'.setup {
-        ensure_installed = { "lua", "python", "norg" },
+        ensure_installed = { "lua", "python", "norg", "query" },
         matchup = { enable = true },
         highlight = { enable = true },
         endwise = {
           enable = true,
         },
+        playground = { enable = true },
         indent = { enable = true },
         refactor = {
           smart_rename = {
@@ -130,7 +150,7 @@ return require('packer').startup(function(use)
           enable = true,
           prev_selection = ',', -- (Optional) keymap to select the previous selection
           keymaps = {
-            ['.'] = 'textsubjects-smart',
+            ['<TAB>'] = 'textsubjects-smart',
             [';'] = 'textsubjects-container-outer',
             ['i;'] = 'textsubjects-container-inner',
           },
@@ -141,10 +161,10 @@ return require('packer').startup(function(use)
             lookahead = true,
             keymaps = {
               -- You can use the capture groups defined in textobjects.scm
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
+              ["am"] = "@function.outer",
+              ["im"] = "@function.inner",
+              ["aM"] = "@class.outer",
+              ["iM"] = "@class.inner",
             },
           },
           swap = {
@@ -186,42 +206,21 @@ return require('packer').startup(function(use)
   use { 'L3MON4D3/LuaSnip',
     config = function()
       local ls = require("luasnip")
-			print('packer luasnip loaded')
-      -- ls.config.set_config({}
-
-      -- press <Tab> to expand or jump in a snippet. These can also be mapped separately
-      -- " via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
-      -- imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
-      -- " -1 for jumping backwards.
-      -- inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
-      --
-      -- snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
-      -- snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
-      --
-      -- " For changing choices in choiceNodes (not strictly necessary for a basic setup).
-      -- imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
-      -- smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
-      -- )
-      vim.keymap.set({ "i", "s" }, "<c-k>", function()
+      vim.keymap.set({ "i", "s" }, "<c-l>", function()
         if ls.expand_or_jumpable() then
           ls.expand_or_jump()
         end
       end, { silent = true })
-
-      vim.keymap.set({ "i", "s" }, "<c-j>", function()
+      vim.keymap.set({ "i", "s" }, "<c-k>", function()
         if ls.jumpable(-1) then
           ls.jump(-1)
         end
       end, { silent = true })
-
-      vim.keymap.set("i", "<c-l>", function()
-        if ls.choice_active() then
-          ls.change_choice(1)
+      vim.keymap.set({ "i", "s" }, "<c-j>", function()
+        if ls.jumpable(1) then
+          ls.jump(1)
         end
       end, { silent = true })
-
-      vim.keymap.set("n", "<leader><leader>s", "source ~/.config/nvim/after/plugin/luasnip.lua<CR>")
-
     end
   }
   use { 'hrsh7th/nvim-cmp',
@@ -233,18 +232,19 @@ return require('packer').startup(function(use)
       'hrsh7th/cmp-nvim-lua',
       'onsails/lspkind-nvim',
       "petertriho/cmp-git",
-      -- completion engine
-      "L3MON4D3/LuaSnip",
+      -- Completion engine
+      'L3MON4D3/LuaSnip',
       "saadparwaiz1/cmp_luasnip",
     },
     config = function()
       -- TODO: add nvim-cmp
-      vim.o.completeopt = 'menu,menuone,noselect'
+      vim.opt.completeopt = { "menu", "menuone", "noselect" }
       local cmp = require 'cmp'
+      local luasnip = require 'luasnip'
       cmp.setup({
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
           end,
         },
         window = {
@@ -255,8 +255,14 @@ return require('packer').startup(function(use)
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({
+          ['<C-e>'] = cmp.mapping(function(fallback)
+            if luasnip.choice_active() then
+              luasnip.change_choice(1)
+            elseif not cmp.abort() then
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<CR>'] = cmp.mapping.confirm({ -- TODO: straighten out e mapping
             select = true,
             behavior = cmp.ConfirmBehavior.Replace,
           }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
@@ -281,6 +287,7 @@ return require('packer').startup(function(use)
 
       -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
       cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
         sources = {
           { name = 'buffer' }
         },
@@ -288,6 +295,7 @@ return require('packer').startup(function(use)
 
       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
       cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
           { name = 'path' },
           { name = 'cmdline', keyword_pattern = [=[[^[:blank:]\!]*]=] }
@@ -334,7 +342,7 @@ return require('packer').startup(function(use)
       }
     },
     config = function()
-      print("nvim-dap config initialated")
+      print("nvim-dap config initialized")
       if not packer_plugins["nvim-dap-python"].loaded then
         require("packer").loader("nvim-dap-python")
       end
@@ -359,7 +367,13 @@ return require('packer').startup(function(use)
     },
     config = function()
       local telescope = require "telescope"
-      telescope.setup {}
+      telescope.setup {
+        extensions = {
+          file_browser = {
+            hijack_netrw = true
+          }
+        }
+      }
       telescope.load_extension "fzf"
       telescope.load_extension "file_browser"
 
@@ -367,7 +381,9 @@ return require('packer').startup(function(use)
     end
   }
   use { "milisims/nvim-luaref" }
+  use { "williamboman/nvim-lsp-installer" }
   use { "neovim/nvim-lspconfig",
+    after = "LuaSnip",
     config = function()
       -- Mappings.
       -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -375,7 +391,7 @@ return require('packer').startup(function(use)
       vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
       vim.keymap.set('n', '<d', vim.diagnostic.goto_prev, opts)
       vim.keymap.set('n', '>d', vim.diagnostic.goto_next, opts)
-      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+      vim.keymap.set('n', '_d', vim.diagnostic.setloclist, opts)
 
       local on_attach = function(client, bufnr)
         -- Enable completion triggered by <c-x><c-o>
@@ -385,24 +401,25 @@ return require('packer').startup(function(use)
         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', '<space>gi', vim.lsp.buf.implementation, bufopts)
         vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
         vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
         vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
         vim.keymap.set('n', '<space>wl', function()
           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
         end, bufopts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set('n', '<space>gt', vim.lsp.buf.type_definition, bufopts)
         vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
         vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-        vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+        vim.keymap.set('n', '<space>gr', vim.lsp.buf.references, bufopts)
+        vim.keymap.set('n', 'gq', vim.lsp.buf.formatting, bufopts)
       end
       -- Add additional capabilities supported by nvim-cmp
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-      require("lspconfig").vimls.setup {
+      local lsp = require("lspconfig")
+      lsp.vimls.setup {
         on_attach = on_attach, -- require("aerial").on_attach,
         capabilities = capabilities,
       }
@@ -482,14 +499,109 @@ return require('packer').startup(function(use)
     end
   }
 
-	use {
-		"AndrewRadev/splitjoin.vim",
-		keys = { "gJ", "gS" },
-	}
+  use {
+    "AndrewRadev/splitjoin.vim",
+    keys = { "gJ", "gS" },
+  }
 
-  -- Automatically set up your configuration after cloning packer.nvim
+  use { "AndrewRadev/switch.vim", keys = { "gs" } }
+  use { "tpope/vim-speeddating", keys = { "<c-a>", "<c-x>" } }
+  use { "nvim-neorg/neorg",
+    -- ft = "norg",
+    -- after = "nvim-treesitter",
+    config = function()
+      require('neorg').setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.norg.dirman"] = {
+            config = {
+              workspaces = {
+                work = "~/Documents/notes/interview_prep",
+                home = "~/Documents/notes/home",
+              }
+            }
+          },
+          ["core.norg.concealer"] = {},
+          ["core.export.markdown"] = {},
+          ["core.norg.journal"] = {},
+          ["core.norg.qol.toc"] = {},
+          ['core.norg.completion'] = {
+            config = {
+              engine = 'nvim-cmp',
+            },
+          },
+        }
+      }
+    end,
+    requires = "nvim-lua/plenary.nvim"
+  }
+  use { "kyazdani42/nvim-web-devicons", config = function() require('nvim-web-devicons').setup() end }
+  use { "catppuccin/nvim", as = "catppuccin",
+    config = function()
+      local catppuccin = require("catppuccin")
+      catppuccin.setup({
+        transparent_background = true,
+        compile = { enable = true },
+        neogit = true,
+        which_key = true,
+        dap = {
+          enabled = true,
+          enable_ui = true,
+        },
+      })
+      vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+      vim.cmd [[colorscheme catppuccin]]
+    end
+  }
+  use {
+    'rcarriga/nvim-notify',
+    config = function()
+      local notify = require 'notify'
+      notify.setup {
+        background_colour = "#000000",
+      }
+    end
+  }
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup {
+        on_attach = function(bufnr)
+          local function map(mode, lhs, rhs, opts)
+            opts = vim.tbl_extend('force', { noremap = true, silent = true }, opts or {})
+            vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
+          map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+
+          -- Actions
+          map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+          map('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+          map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+          map('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+          map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+          map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+          map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+          map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+          map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+          map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+          map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
+          map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+          map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
+
+          -- Text object
+          map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+          map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end
+      }
+    end
+  } -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
     require('packer').sync()
   end
 end)
+
+-- TODO: make an autocmd or make a file to set the lua tab widths
